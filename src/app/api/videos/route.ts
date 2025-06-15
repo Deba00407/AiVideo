0e74843a619e2afa9e150af91a47d0612ffe89be
Deba00407 <cdebanjan456@gmail.com>
2025-06-15 23:06:37 +0530

    Minor error fix

--- src/app/api/videos/route.ts
+++ src/app/api/videos/route.ts
@@ -26,6 +26,7 @@ export async function GET() {
         }, { status: 200 })
 
     } catch (error) {
+        console.error(error)
         return NextResponse.json({
             "message": "Failed to get videos data"
         }, { status: 500 })
